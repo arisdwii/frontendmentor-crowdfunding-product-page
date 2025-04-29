916d2611c1758fa792703c76d83e54f7a9b9617b
arisdwii <143518572+arisdwii@users.noreply.github.com>
2025-04-29 21:08:49 +0700

    Update modalSuccess.js

--- javascript/modules/components/modalSuccess.js
+++ javascript/modules/components/modalSuccess.js
@@ -3,7 +3,7 @@ const btnCloseSuccess = document.querySelector(".btn-close");
 
 function locationHref(element) {
   element.addEventListener("click", () => {
-    location.href = "frontendmentor-crowdfunding-product-page/index.html";
+    location.href = "/frontendmentor-crowdfunding-product-page/index.html";
   });
 }
 
